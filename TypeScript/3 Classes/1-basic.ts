ea17a54473dd34c9b432f09e081b8d9e7eea6772
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-01 17:53:39 +0530

    Update 1-basic.ts

--- TypeScript/3 Classes/1-basic.ts
+++ TypeScript/3 Classes/1-basic.ts
@@ -12,9 +12,9 @@
 
         // constructor
 
-        constructor(name: string, age: number) {
-            this.name = name;
-            this.age = age;
+        constructor(n: string, a: number) {
+            this.name = n;
+            this.age = a;
         }
 
         // method
