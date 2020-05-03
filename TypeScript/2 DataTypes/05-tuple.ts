1659315b299e6b91128f0c2e33e9b01cb7dd5f5e
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-03 15:54:16 +0530

    Delete 05-tuple.ts

--- TypeScript/2 DataTypes/05-tuple.ts
+++
@@ -1,8 +0,0 @@
-
-// Tuples - array with fixed type and fixed number of children
-
-let myData: [String, number, boolean] = ['Akhil', 24, true];
-
-// let myData: [String, number, boolean] = ['Akhil', 24];  ----------- Error
-
-// let myData: [String, number] = ['Akhil', 24, true];     ------------ Error
