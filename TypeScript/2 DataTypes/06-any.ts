77361eb620e89f8a74e7220b1c51d3cd041317aa
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-03 15:54:53 +0530

    Delete 06-any.ts

--- TypeScript/2 DataTypes/06-any.ts
+++
@@ -1,8 +0,0 @@
-
-// any - for dynamic variables(variables can hold any typed value)
-
-let x: any;
-
-x = 'Helllo typescript';                  // x hold string
-x = 10;                                  // x hold number
-x = true;                                // x hold boolean
