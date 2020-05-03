fa24a3801a98d5ff16ff1dc8dd4b195ca0011a6a
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-03 16:05:02 +0530

    Add files via upload

---
+++ 02 datatypes/03-boolean.ts
@@ -0,0 +1,4 @@
+
+// Boolean can hold 2 values , true or false
+
+let currentStatus: boolean = true;
---
+++ 02 datatypes/09-never.ts
@@ -0,0 +1,9 @@
+
+// never  - common use with function to define function never return 
+
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+
