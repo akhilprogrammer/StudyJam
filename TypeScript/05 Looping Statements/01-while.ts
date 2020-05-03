c5dba67df178bb51c494afbc6dccc5dd98c72a37
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-03 18:30:12 +0530

    Add files via upload

---
+++ TypeScript/05 Looping Statements/01-while.ts
@@ -0,0 +1,32 @@
+
+// while loop 
+
+    // while loop first check condition
+    // if condition true then body execute. this repeat until condition become false
+    // when condition become false the loop exit
+
+    let x: number = 1;
+
+    while(x <= 10) {
+
+        console.log(x);      
+        x = x + 1;
+
+    }
+
+
+// do - while loop
+
+    // do while loop first execute body
+    // then check condition. if condition true repeat the process
+    // if condition false loop exit
+
+    let y: number = 1;
+
+    do {
+
+        console.log(y);     
+        y = y + 1;
+
+    } 
+    while(y <= 10);
---
+++ TypeScript/05 Looping Statements/02-for.ts
@@ -0,0 +1,39 @@
+
+
+// for loop
+
+    let firstArray = [1, 2, 3, 4, 5];
+
+    for(let i = 0; i < firstArray.length; i++) {
+
+        console.log(firstArray[i]);  
+
+    }
+
+
+
+// for - of loop
+
+
+    // for - of loop iterarte based on length of array
+    // on each iteration variable x hold value of each position of array in order from first to last
+
+    let secondArray = [6, 7, 8, 9, 10];
+
+    for(let x of secondArray) {
+        console.log(x);
+    }
+
+
+
+// for  - in loop
+
+    // for - in loop iterarte based on length of array
+    // on each iteration variable x hold index of iterartion
+    
+    let thirdArray = [11, 12, 13, 14, 15];
+
+    for(let x in thirdArray) {
+        console.log(thirdArray[x]);
+    }
+    
