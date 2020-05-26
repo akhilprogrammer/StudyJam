8240996cf00cea403ce717a2fe2748613c30ae1d
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-26 20:50:38 +0530

    Add files via upload

---
+++ Dart/02 Variable/01-variable_declaration.dart
@@ -0,0 +1,16 @@
+
+
+void main() {
+
+  // we can declare a variable simply using var keyword
+
+    var x = 10;
+
+
+  // also we can declare a variable by explicitly saying its type
+
+    int no = 10;
+
+}
+
+
