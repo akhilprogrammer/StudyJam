a7520dc03b42e1fc9af8b5646071aef2be86e6e9
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-05-27 14:43:29 +0530

    Update 04-list.dart

--- Dart/03 Data Types/04-list.dart
+++ Dart/03 Data Types/04-list.dart
@@ -3,11 +3,13 @@
 void main() {
 
   // List type  - represent collection of data
+  // Element of a List can be access via index(position)
 
     List numbers = [1, 2, 3, 4, 5];
     List friends = ['John', 'Michel', 'Smith'];
 
-    print(numbers);
+    print(numbers);       // print whole list
+    print(number[0]);     // print 1st elemnt. index start from 0
 
     numbers.add(6);       // adding a data into list
     numbers.remove(2);   // removing a data from list
