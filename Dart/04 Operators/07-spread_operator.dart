1c84e5f2a1371bf4134d5d0e69f23667b0c8808b
akhilprogrammer <39589942+akhilprogrammer@users.noreply.github.com>
2020-06-04 19:33:49 +0530

    Update 07-spread_operator.dart

--- Dart/04 Operators/07-spread_operator.dart
+++ Dart/04 Operators/07-spread_operator.dart
@@ -4,11 +4,12 @@
 
 void main() {
 
-  // we create a collection named x
+  // we create  collections named x and y
 
   List x = [1,2,3,4,5];
   List y = [10,11,12,13,14,15];
-  // we insert all the elemnts of x into another collection named y using spread opeator
+  
+  // we insert all the elemnts of x and y into another collection named z using spread opeator
 
   List z = [...x, 6, 7, 8, 9,...y,16,17,18];
 
